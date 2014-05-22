c89a814724b45b83402fb348b4b519e765430546
Stephen Cleary <liveid.20.stephen-cleary@spamgourmet.com>
2014-05-22 12:43:03 -0400

    Delete the solar system.

--- setpath.ps1
+++
@@ -1,4 +0,0 @@
-$dir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-
-$env:PATH += ";" + $dir + "\ruby\bin;" + $dir + "\devkit\bin;" + $dir + "\git\bin;" + $dir + "\Python\App;" + $dir + "\Python\App\Scripts;" + $dir + "\devkit\mingw\bin;" + $dir + "\curl\bin"
-$env:SSL_CERT_FILE = $dir + "\curl\bin\cacert.pem"
