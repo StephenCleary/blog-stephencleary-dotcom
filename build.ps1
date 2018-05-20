90fc059fa230766a6ce3408b1641dd0fd87b1e13
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-05-19 23:11:28 -0400

    Move to VSCode as a build system.

--- build.ps1
+++
@@ -1,4 +0,0 @@
-cd blog
-jekyll build --config _config.www.yml
-jekyll build --config _config.blog.yml
-cd ..
--- serve.ps1
+++
@@ -1,4 +0,0 @@
-cd blog
-Start-Process jekyll -ArgumentList "serve --config _config.www.yml,_config.www.local.yml -w"
-Start-Process jekyll -ArgumentList "serve --config _config.blog.yml,_config.blog.local.yml -w"
-cd ..
